cad4a018068cbcbbfd048247b3d95e328a974f08
Atin Gupta <atingupta@email.com>
2023-03-27 13:36:07 +0530

    -

--- Hands-on/Day2/01-Connect-to-AKS-Cluster/optional/01-Install-Azure-CLI.sh
+++
@@ -1,7 +0,0 @@
-#curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
-
-az login -u <username> -p <password>
-
-az account list
-
-az account set --subscription="cab7799e-bdd5-4b31-a003-c56612e38f3e"
--- Hands-on/Day2/01-Connect-to-AKS-Cluster/optional/03-create-aks.sh
+++
@@ -1,8 +0,0 @@
-AKS_NAME="akstrng"
-AKS_RG="rgtrngaks"
-mylocation="EASTUS"
-
-az group create --name $AKS_RG --location $mylocation
-
-az aks create --resource-group $AKS_RG --name $AKS_NAME --node-count 1  --load-balancer-sku basic --node-vm-size Standard_B2s --network-plugin azure --network-policy calico  --enable-managed-identity  --generate-ssh-keys --location $mylocation
-
